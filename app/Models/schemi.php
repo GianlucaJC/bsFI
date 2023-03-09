c65f32869c8ce94ad6901219eca72a14b80c778a
GianlucaJC <morescogianluca@gmail.com>
2023-03-09 10:58:25 +0100

    Implement Spatie

---
+++ app/Models/schemi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class schemi extends Model
+{
+	protected $table="schemi";
+    use HasFactory;
+}
